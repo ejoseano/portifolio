2516b4607ab8613b4a70aac759bd6ae40ed07827
Joseano Eduardo <ejoseano@hotmail.com>
2021-03-03 11:56:42 -0400

    v3

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,5 @@
-import { useState } from 'react';
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador +1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )    
-}
-
-function pagina() {
-return <Contador />
-}
-
+    return <h1>Home</h1>
+ 
 export default Home
